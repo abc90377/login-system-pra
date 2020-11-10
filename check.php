bcf8ecde53b56ce0653b0447e7ab6ade69742185
abc90377 <abc870710@gmail.com>
2020-11-10 22:58:45 +0800

    完成將會員導向不同頁面的功能

--- check.php
+++ check.php
@@ -7,7 +7,33 @@
  * 5. 檢查會員身份及權限
  * 6. 依據會員身份導向不同的頁面
  */
+$dsn="mysql:host=localhost;dbname=login_pra;charset=utf8";
+$pdo=new PDO($dsn,'root','');
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+$sql="select * from `login` where `acc`='$acc' && `pw`='$pw'";
+$check=$pdo->query($sql)->fetch();
+$rolecheck="select * from `login`,`mem` where `login`.`acc`='$acc' && `login`.`id`=`mem`.`login_id`";
+$user=$pdo->query($rolecheck)->fetch();
+$role=$user['role'];
+$name=$user['name'];
+if ($check) {
+    switch ($role) {
+        case '會員':
+            header("location:mem.php?name=$name");
+            break;
+        case '管理員':
+            header("location:admin.php?name=$name");
+            break;
+        case 'vip':
+            header("location:vip.php?name=$name");
+            break;
+        
 
+    }
+}else {
+    header('location:index.php?meg=帳號或密碼錯誤');
+}
 
 
 ?>
